7231d0691e25f07abf5a6aa95a095bc394b68541
Kirill Kuprikov <kkuprikov@gmail.com>
2017-05-14 12:59:23 +0300

    syntax err

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,7 +55,7 @@ class User < ApplicationRecord
     end
 
     if params[:career]
-      where_clause += " and career like '%position"="#{params[:career]}%'"
+      where_clause += " and career like '%position\"=\"#{params[:career]}%'"
     end
 
     if !where_clause.blank?
