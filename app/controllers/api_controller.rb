1981f84fe4281cb98e8b9527d7ee78f1101311f7
Kirill Kuprikov <kkuprikov@gmail.com>
2017-05-15 16:27:01 +0300

    Return cities by country_id

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -15,7 +15,7 @@ class ApiController < ApplicationController
   end
 
   def cities
-    res = get_dictionary("city_id", "city_name")
+    res = params[:country_id] ? get_dictionary("city_id", "city_name", "country_id=toString(#{params[:country_id]})") : ""
     respond_to do |format|
       format.json { render json: JSON.parse(res) }
     end
@@ -30,12 +30,12 @@ class ApiController < ApplicationController
 
   private
 
-  def get_dictionary id_column, data_column
-    query = "select distinct #{id_column}, #{data_column} from vk.users where #{data_column}!='' format JSON"
+  def get_dictionary id_column, data_column, where_clause=""
+    query = "select distinct #{id_column}, #{data_column} from vk.users where #{data_column}!=''"
+    query += " AND #{where_clause}" if !where_clause.blank?
+    query += " format JSON"
     Rails.logger.error query
-    res = Typhoeus.post('http://localhost:8123/', body: query).response_body
-    Rails.logger.error res
-    res
+    Typhoeus.post('http://localhost:8123/', body: query).response_body
   end
 
 end
