e1fd0b1df9370e3f57783e6aec462f9988072a60
Kirill Kuprikov <kkuprikov@gmail.com>
2017-05-15 16:03:57 +0300

    Trying ro fix encoding

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,23 +8,23 @@ class ApiController < ApplicationController
   end
 
   def countries
-    get_dictionary("country_id", "country_title")
+    res = get_dictionary("country_id", "country_title")
     respond_to do |format|
-      format.json { render json: res.to_json }
+      format.json { render json: JSON.parse(res) }
     end
   end
 
   def cities
-    get_dictionary("city_id", "city_name")
+    res = get_dictionary("city_id", "city_name")
     respond_to do |format|
-      format.json { render json: res.to_json }
+      format.json { render json: JSON.parse(res) }
     end
   end
 
   def universities
     res = get_dictionary("university", "university_name")
     respond_to do |format|
-      format.json { render json: res.to_json }
+      format.json { render json: JSON.parse(res) }
     end
   end
 
@@ -33,7 +33,9 @@ class ApiController < ApplicationController
   def get_dictionary id_column, data_column
     query = "select distinct #{id_column}, #{data_column} from vk.users where #{data_column}!=''"
     Rails.logger.error query
-    Typhoeus.post('http://localhost:8123/', body: query).response_body
+    res = Typhoeus.post('http://localhost:8123/', body: query).response_body
+    Rails.logger.error res
+    res
   end
 
 end
